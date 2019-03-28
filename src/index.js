57ba646278c902d5f5b233573aa4283ef41468b5
Nikita Levkovich <mikita.liaukovich@gmail.com>
2019-03-29 02:59:30 +0300

    refactor: rewrite style using mixins

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import './index.pug';
 import './index.scss';
 import { $, inner } from './utils';
 import * as data from './data.json';
- 
+
+const randomPic = function() {
+  return(Math.floor(Math.random() * (13)) + 1);
+}
+
 data.forEach(el => {
   let tag = `
-  <div class='item' style="background: url('./assets/backg${Math.floor(Math.random() * (13)) + 1}.png'); background-size: cover;">
+  <div class='item' style="background: url('./assets/backg${randomPic()}.png'); background-size: cover;">
     <h2>${el.title}</h2>
     <p>${el.description}</p>
   </div>`
