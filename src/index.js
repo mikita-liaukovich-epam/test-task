aae9dbf1386878db5b62a599d841f86916c8be0c
Nick Levkovich <mikita.liaukovich@gmail.com>
2019-03-30 13:58:09 +0300

    fix: background pictures for items

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './index.pug';
 import './index.scss';
 import { $, inner } from './utils';
+import { isTSEnumMember } from 'babel-types';
 
 const randomPic = function() {
   return(Math.floor(Math.random() * (13)) + 1);
 }
 
 let items = document.getElementsByClassName('item');
-items.forEach(element => {
-  console.log(element);  
-});
+for(let item of items) {
+  console.log(item);
+  item.style.background = `url('./assets/backg${randomPic()}.png')`;
+  item.style.backgroundSize = 'cover';
+}
