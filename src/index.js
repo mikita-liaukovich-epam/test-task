21e786b71aa683311233775fecaf23d22126a01e
Nick Levkovich <mikita.liaukovich@gmail.com>
2019-03-30 13:51:13 +0300

    feat: data in pug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import './index.pug';
 import './index.scss';
 import { $, inner } from './utils';
-import * as data from './data.json';
 
 const randomPic = function() {
   return(Math.floor(Math.random() * (13)) + 1);
 }
 
-data.forEach(el => {
-  let tag = `
-  <div class='item' style="background: url('./assets/backg${randomPic()}.png'); background-size: cover;">
-    <h2>${el.title}</h2>
-    <p>${el.description}</p>
-  </div>`
-  inner($('.container'), tag);
-})
+let items = document.getElementsByClassName('item');
+items.forEach(element => {
+  console.log(element);  
+});
